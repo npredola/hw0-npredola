714bce25b44c709a1c5857cfedccf9b014a9337a
Nikola Predolac <npredola@cs.cmu.edu>
2012-09-09 00:16:30 -0400

    Commit after correcting the style of the Stanford-Tokenizer-based code.

--- src/main/java/DependencyExample.java
+++ src/main/java/DependencyExample.java
@@ -6,26 +6,22 @@ import edu.stanford.nlp.process.Tokenizer;
 
 /**
  * An example for Homework 0 of 11791 F12
- *
+ * 
  * @author Zi Yang <ziy@cs.cmu.edu>
  */
 
 public class DependencyExample {
 
   /**
-   * Tokenize a sentence in the argument, and print out
-   * the tokens to the console.
-   *
+   * Tokenize a sentence in the argument, and print out the tokens to the console.
+   * 
    * @param args
-   *          Set the first argument as the sentence to
-   *          be tokenized.
-   *
+   *          Set the first argument as the sentence to be tokenized.
+   * 
    */
   public static void main(String[] args) {
-    TokenizerFactory<Word> factory =
-      PTBTokenizerFactory.newTokenizerFactory();
-    Tokenizer<Word> tokenizer =
-      factory.getTokenizer(new StringReader(args[0]));
+    TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
     System.out.println(tokenizer.tokenize());
   }
 
